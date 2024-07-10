38538f1f733b50fda0a3bf56f80e695678c473f8
Mykola Shchypailo <kolykol8787@gmail.com>
2024-07-10 17:07:03 +0300

    init commit

--- src/main/java/spring/security/jwt/IamServiceApplication.java
+++ src/main/java/spring/security/jwt/IamServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SecurityJwtApplication {
+public class IamServiceApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SecurityJwtApplication.class, args);
+		SpringApplication.run(IamServiceApplication.class, args);
 	}
 }
